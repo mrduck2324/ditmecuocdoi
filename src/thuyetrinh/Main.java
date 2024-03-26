fc2d5065d8a7494c992daa977c5c6333ea25ccaf
ThanhTinhYarushi <thanhtinh.yarushi@gmail.com>
2024-03-26 08:54:51 +0700

    fix xong

--- src/thuyetrinh/Main.java
+++ src/thuyetrinh/Main.java
@@ -2,14 +2,18 @@ package thuyetrinh;
 
 public class Main {
     public static void main(String[] args) {
-        controlroom danhsach = new controlroom();
-        danhsach.nhap();
-        danhsach.xuat();
-        lab test = new lab();
-        test.nhap();
-        test.xuat();
-        Scientist key = new Scientist("nguyen van A");
+        // controlroom danhsach = new controlroom();
+        // danhsach.nhap();
+        // danhsach.xuat();
+        // lab test = new lab();
+        // test.nhap();
+        // test.xuat();
 
-
+        /**
+         * * sau hàm new là constructor, ...
+         * * ngày sinh nên viết class riêng, hoặc dùng class Java Date, vd dùng ngày 17 thay cho tham số dateBirth
+         */
+        nhanvien scientist = new Scientist("John Doe", 1990, "PhD", "Researcher", 5, 25, 10000.0);
+        
     }
-    }
+}
--- src/thuyetrinh/Scientist.java
+++ src/thuyetrinh/Scientist.java
@@ -1,15 +1,38 @@
 package thuyetrinh;
 
-public abstract class Scientist extends nhanvien {
+// ! Lớp con của abstract của lớp nhân viên m ghi thêm abstract vô chi v ông cố nội,
+    //!! {ABSTRACT CLASS KHÔNG THỂ KHƠI TẠOOO!!}
+    // * má ngồi sửa cho ông cố ổng ghi abstract class mới ghê chứ
+public class Scientist extends nhanvien {
     public int article; //so bai bao cua nha khoa hoc
     public int songaychamcong;
     public double bacluong;//bac luong la so thap phan dung double
+
+    // ! Constructor
+    // /**
+    //  * có 7 tham số thì bên kia cũng phải có 7 tham số, hiểu không em ??
+    //  * @param name
+    //  * @param datebirth
+    //  * @param license
+    //  * @param position
+    //  * @param article
+    //  * @param songaychamcong
+    //  * @param bacluong
+    //  */
     public Scientist(String name, int datebirth, String license, String position, int article, int songaychamcong, double bacluong) {
         super(name,datebirth,license,position);
         this.article = article;
         this.songaychamcong = songaychamcong;
         this.bacluong = bacluong;
     }
+
+    @Override
+    public void nhap() {}
+
+    @Override
+    public void xuat() {}
+
+    @Override
     public double luong() {
         return songaychamcong * bacluong;
     }
